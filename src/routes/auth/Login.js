b8d09dd2fbdd218c6861e0c9aaee3305367c131a
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-03-11 18:19:50 -0500

    added imports

--- src/routes/auth/Login.js
+++ src/routes/auth/Login.js
@@ -1,15 +1,27 @@
-// src/routes/auth/Login.js
-import React from 'react';
-import './auth.css';
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/User')
+const { comparePassword } = require('../../utils/password')
+const { generateToken } = require('../../utils/tokens')
 
-function Login({ username, password, setUsername, setPassword }) {
-  return (
-    <form>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button type="submit">Log In</button>
-    </form>
-  );
-}
+router.post('/', async (req, res) => {
+    User.findOne({ where: { username: req.body.username } })
+        .then(user => {
+            comparePassword(req.body.password, user.password)
+                .then(correct => {
+                    if (correct) {
+                        generateToken(user.id, user.username)
+                            .then(token => res.send({
+                                message: 'Logged in successfully',
+                                accessToken: token,
+                            }))
+                            .catch(err => res.status(500).send(err))
+                    } else {
+                        res.status(401).send({ message: 'Incorrect credentials' })
+                    }
+                })
+        })
+        .catch(() => res.status(401).send({ message: 'Incorrect credentials' }))
+})
 
-export default Login;
+module.exports = router
--- src/routes/auth/Register.js
+++ src/routes/auth/Register.js
@@ -1,15 +1,22 @@
-// src/routes/auth/Register.js
-import React from 'react';
-import './auth.css';
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/User')
+const { generateToken } = require('../../utils/tokens')
 
-function Register({ username, password, setUsername, setPassword }) {
-  return (
-    <form>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button type="submit">Sign Up</button>
-    </form>
-  );
-}
+router.post('/', async (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+    })
+        .then(data => {
+            generateToken(data.id, data.username)
+                .then(token => res.send({
+                    message: 'Registered successfully',
+                    accessToken: token,
+                }))
+                .catch(err => res.status(500).send(err))
+        })
+        .catch(() => res.status(400).send({ message: 'Invalid username' }))
+})
 
-export default Register;
+module.exports = router
