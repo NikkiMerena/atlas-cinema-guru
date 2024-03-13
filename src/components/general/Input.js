39020732e7573774098d14ea257049664dad70ef
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-03-13 15:00:20 -0500

    added css

--- src/components/general/Input.js
+++ src/components/general/Input.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import './general.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-function Input({ label, type, className, value, setValue, icon, inputAttributes }) {
+function Input({ label, type, className, value, setValue, icon, inputAttributes, togglePassword, isPasswordVisible, showPasswordToggle }) {
   // Handle input changes, call setValue w/ new value
   const handleInput = (event) => {
     setValue(event.target.value);
   };
 
+  const inputType = type === 'password' && isPasswordVisible ? 'text' : type;
+
   return (
     <div className={`input-group ${className || ''}`}>
-      {label && <label>{label}</label>}
+      <div className="label-icon-wrapper">
+        {icon && <FontAwesomeIcon icon={icon} className="icon-default" />}
+        {label && <label>{label}</label>}
+      </div>
       <div className="input-wrapper">
-        {icon && <FontAwesomeIcon icon={icon} />}
         <input
-          type={type}
+          type={inputType}
           value={value}
           onChange={handleInput}
           {...inputAttributes}
         />
+        {showPasswordToggle && type === 'password' && (
+          <FontAwesomeIcon
+            icon={isPasswordVisible ? faEye : faEyeSlash}
+            onClick={togglePassword}
+            className="toggle-password-icon"
+          />
+        )}
       </div>
     </div>
   );
