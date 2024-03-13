c3d510345c07ed4f225a7dc322bd6d36a6fcb8e7
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-03-12 21:22:12 -0500

    part of task 7

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import Header from '../navigation/Header';
 import './dashboard.css';
+import Header from '../../components/navigation/Header';
+import SideBar from '../../components/navigation/SideBar';
+import Favorites from './Favorites';
+import HomePage from './HomePage';
+import WatchLater from './WatchLater';
 
-function Dashboard({ userUsername, setIsLoggedIn }) {
 
-  return (
-    <div className="dashboard">
-      <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-    </div>
-  );
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+const Dashboard = ({userUsername, setIsLoggedIn}) => {
+    return (
+        <BrowserRouter>
+        <div className='dashboard-div'>
+            <Header
+                userUsername={userUsername}
+                setIsLoggedIn={setIsLoggedIn}
+            />
+            <SideBar />
+            <Routes>
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/favorites' element={<Favorites />} />
+                <Route path='/watchlater' element={<WatchLater />} />
+                <Route path='*' element={<Navigate to='/home' />} />
+            </Routes>
+        </div>
+        </BrowserRouter>
+    )
 }
 
 export default Dashboard;
